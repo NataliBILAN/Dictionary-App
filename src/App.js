84aad381d2fd64da686cbb29d4f012879abb18dd
Natali_Bilan <natalia.bilan@datassential.com>
2022-07-16 19:03:08 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dictionary-wrapper">
+      <TextField id="outlined-basic" label="Type a word" variant="outlined" />
+      <Button color="secondary">Search</Button>
     </div>
   );
 }
