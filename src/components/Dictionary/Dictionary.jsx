807f1d438a0d792284beed7657a53b4bdf04ab7d
Natali_Bilan <natalia.bilan@datassential.com>
2022-07-18 00:14:45 +0300

    added dictionary API

--- src/components/Dictionary/Dictionary.jsx
+++ src/components/Dictionary/Dictionary.jsx
@@ -1,30 +1,60 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 export default function Dictionary() {
   const [searchValue, setSearchValue] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [data, setData] = useState(null);
+
+  const search = () => {
+    axios
+      .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`)
+      .then((response) => setData(response.data[0]));
+  };
 
   const handleSearchButtonClick = (event) => {
     event.preventDefault();
-    console.log("searchValue", searchValue);
+    search();
+  };
+
+  const handleEnterClick = ({ key }) => {
+    if (key === "Enter") {
+      search();
+    }
   };
 
   const handleTextFieldChange = ({ target }) => {
     setSearchValue(target.value);
   };
-  
+
+  useEffect(() => {
+    if (isSubmitting && searchValue) {
+      search();
+      setIsSubmitting(false);
+    }
+  }, [search, isSubmitting]);
+
+  console.log("data", data);
+
   return (
     <div>
       <TextField
-        id="outlined-basic"
         label="Type a word"
-        variant="outlined"
+        variant="filled"
+        type="search"
+        autoComplete="off"
         value={searchValue}
         onChange={handleTextFieldChange}
+        onKeyDown={handleEnterClick}
       />
-      <Button color="secondary" onClick={handleSearchButtonClick}>
+      <Button
+        color="secondary"
+        variant="contained"
+        onClick={handleSearchButtonClick}
+      >
         Search
       </Button>
     </div>
